e9ced9cbf089e53f54dd0966d0473e4edf0658bf
royankit11 <royankit11@gmail.com>
2023-09-26 18:23:05 -0400

    Initial Commit

---
+++ GitDemo/GitDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitDemoApp.swift
+//  GitDemo
+//
+//  Created by Ryan O’Meara on 9/26/23.
+//
+
+import SwiftUI
+
+@main
+struct GitDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GitDemo/Rik.swift
@@ -0,0 +1,8 @@
+//
+//  Rik.swift
+//  GitDemo
+//
+//  Created by Rik Roy on 9/26/23.
+//
+
+import Foundation
