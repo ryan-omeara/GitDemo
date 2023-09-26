674f51dfbcb3829f2a73772bb7e6530e7a7e46d5
yulkim19 <juliayulkim@gmail.com>
2023-09-26 18:39:09 -0400

    created a file

---
+++ GitDemo/Yul.swift
@@ -0,0 +1,20 @@
+//
+//  Yul.swift
+//  GitDemo
+//
+//  Created by Julia Kim on 9/26/23.
+//
+
+import SwiftUI
+
+struct Yul: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Yul_Previews: PreviewProvider {
+    static var previews: some View {
+        Yul()
+    }
+}
